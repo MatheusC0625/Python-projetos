1fa83c26457dcf68e8fb0b9dc7461920694018cb
Matheus-ai-master <stsycosta@gmail.com>
2026-07-01 20:13:19 -0300

    corrige o problema de menssagem de ganhador

--- Blackjack.py
+++ Blackjack.py
@@ -19,27 +19,25 @@ print("Mão do dealer:", mao_dealer[0], "e uma carta escondida.")
 while sum(mao_jogador) < 21:
     pergunta = input("Pegar uma carta ou parar? (pegar/parar)")
     if pergunta == "pegar":
-     mao_jogador.append(cartas.pop())
-     print("Sua mão:", sum(mao_jogador))
+        mao_jogador.append(cartas.pop())
+        print("Sua mão:", sum(mao_jogador))
     elif pergunta == "parar":
-     print("Você parou, sua mão:", sum(mao_jogador))
-     break
+        print("Você parou, sua mão:", sum(mao_jogador))
+        break
     else:
-      print("Comando inválido!") 
+        print("Comando inválido!")
 
 if sum(mao_jogador) > 21:
-  print("Você perdeu!")
+    print("Você perdeu!")
 else:
-  mao_dealer.append(cartas.pop())
+    while sum(mao_dealer) < 17:
+        mao_dealer.append(cartas.pop())
 
-while sum(mao_dealer) < 17:
-  mao_dealer.append(cartas.pop())
-
-if sum(mao_dealer) > 21:
-  print(f"Você ganhou com a mão de {sum(mao_jogador)} e o dealer perdeu com a mão de {sum(mao_dealer)}. Parabéns!")
-elif sum(mao_dealer) < sum(mao_jogador):
-  print(f"Você ganhou com a mão de {sum(mao_jogador)} e o dealer com perdeu com a mão de {sum(mao_dealer)}. Parabéns!")
-elif sum(mao_dealer) > sum(mao_jogador):
- print(f"O dealer ganhou com a mão de {sum(mao_dealer)} em cima da sua mão de {sum(mao_jogador)}")
-else:
-  print("Empate!")
+    if sum(mao_dealer) > 21:
+        print(f"Você ganhou com a mão de {sum(mao_jogador)} e o dealer perdeu com a mão de {sum(mao_dealer)}. Parabéns!")
+    elif sum(mao_dealer) < sum(mao_jogador):
+        print(f"Você ganhou com a mão de {sum(mao_jogador)} e o dealer perdeu com a mão de {sum(mao_dealer)}. Parabéns!")
+    elif sum(mao_dealer) > sum(mao_jogador):
+        print(f"O dealer ganhou com a mão de {sum(mao_dealer)} em cima da sua mão de {sum(mao_jogador)}")
+    else:
+        print("Empate!")
