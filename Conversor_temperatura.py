9a768ae772a6b87afe47a14cd9a5da6f2f71d20f
MatheusC0625 <stsycosta@gmail.com>
2026-07-25 03:37:36 -0300

    Apenas algo que aprendi para deixar a resposta do sistema mais descritiva

--- Conversor_temperatura.py
+++ Conversor_temperatura.py
@@ -5,26 +5,26 @@ escolha = int(input(f"1 - Celsius para Fahrenheit\n2 - Celsius para Kelvin\n3 - 
 if escolha == 1:
     c = float(input("Celsius: "))
     x = (c * 9/ 5) + 32
-    print(f"Convertendo: {x}")
+    print(f"Convertendo: {x:.2f}")
 elif escolha == 2:
     c = float(input("Celsius: "))
     x = c + 273.15
-    print(f"Convertendo: {x}")
+    print(f"Convertendo: {x:.2f}")
 elif escolha == 3:
     f = float(input("Fahrenheit: "))
     x = (f - 32) * 5/9
-    print(f"Convertendo: {x}")
+    print(f"Convertendo: {x:.2f}")
 elif escolha == 4:
     f = float(input("Fahrenheit: "))
     x = (f - 32) * 5/9 + 273.15
-    print(f"Convertendo: {x}")
+    print(f"Convertendo: {x:.2f}")
 elif escolha == 5:
     k = float(input("Kelvin: "))
     x = k - 273.15
-    print(f"Convertendo: {x}")
+    print(f"Convertendo: {x:.2f}")
 elif escolha == 6:
     k = float(input("Kelvin: "))
     x = (k - 273.15) * 9/5 + 32
-    print(f"Convertendo: {x}")
+    print(f"Convertendo: {x:.2f}") #APRENDI COM O CLAUDE COMO DEIXAR AS CASAS DECIMAIS CERTINHAS, tentar lembrar daqui 
 else:
     print("Opção inválida! Tente novamente.")
