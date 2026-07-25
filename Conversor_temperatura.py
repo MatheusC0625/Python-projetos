fafbff257e67e551c4fce838573e9f9c8e0a0c1b
MatheusC0625 <stsycosta@gmail.com>
2026-07-25 03:35:18 -0300

    apenas o menu de escolhas

---
+++ Conversor_temperatura.py
@@ -0,0 +1,3 @@
+print("===Conversor de Temperatura===")
+
+escolha = int(input(f"1 - Celsius para Fahrenheit\n2 - Celsius para Kelvin\n3 - Fahrenheit para Celsius\n4 - Fahrenheit para Kelvin\n5 - Kelvin para Celsius\n6 - Kelvin para Fahrenheit\n"))
