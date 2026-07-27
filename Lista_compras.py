285bd7ce6e008f835408218b7d2265e148abee7b
Matheus-ai-master <stsycosta@gmail.com>
2026-07-26 23:22:54 -0300

    minor changes depois de rodar o sistema

--- Lista_compras.py
+++ Lista_compras.py
@@ -2,21 +2,23 @@ print("===Lista de compras===")
 
 lista = []
 total = 0
-escolha = int(input(f"Escolha uma opção\n1 - Adicionar produto\n2 - Ver lista\n3 - Ver total\n4 - Remover produto\n5 - Sair"))
 
 while True:
-   escolha = int(input(f"Escolha uma opção\n1 - Adicionar produto\n2 - Ver lista\n3 - Ver total\n4 - Remover produto\n5 - Sair"))
+   escolha = int(input(f"Escolha uma opção\n1 - Adicionar produto\n2 - Ver lista\n3 - Ver total\n4 - Remover produto\n5 - Sair\n"))
+                       
    if escolha == 1:
     produto = input("Nome do produto: ")
     lista.append(produto)
-    preco = float("Valor do produto: ")
+    preco = float(input("Valor do produto: "))
     total = preco + total
    elif escolha == 2:
-    print(f"Lista de compras até o momento: \n{lista}")
+    for item in lista:
+     print(item)
    elif escolha == 3:
-    print(f"Total da lista de compras até agora: \n{total}")
+    print(f"Total da lista de compras até agora: \n{total:.2f}")
    elif escolha == 4:
-    print(f"Lista de compras ate o momento: {lista}")
+    for item in lista:
+     print(item)
     produto = input("Nome do produto que deseja remover: ")
     lista.remove(produto)
    elif escolha == 5:
